40eaada2dd60ea820d53d7f33a55a027812bfbb8
diego <diego.a.gonzalezf@gmail.com>
2022-09-22 01:35:07 -0300

    Version0.2 Login Page and Main Menu

--- app/src/main/java/com/example/whereproject/App.java
+++ app/src/main/java/com/example/whereproject/App.java
@@ -2,7 +2,14 @@ package com.example.whereproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class App extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class App extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        ImageButton boton = findViewById(R.id.botonUbi);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(boton, "Enviar Alerta", Snackbar.LENGTH_LONG)
+                        .setAction("ALERTA!", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        {
+                                            final VibrationEffect vibrationEffect1;
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                                vibrationEffect1 = VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE);
+                                            }
+                                        }
+                                    }
+                                }
+                        ).show();
+            }
+            });
     }
 }
