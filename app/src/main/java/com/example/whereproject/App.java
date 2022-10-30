88dcae5c0a4bdfe8c404533e307db0aa57b80adb
diego <diego.a.gonzalezf@gmail.com>
2022-10-29 22:05:48 -0300

    Version0.4 maps and firebase

---
+++ app/src/main/java/com/example/whereproject/App.java
@@ -0,0 +1,38 @@
+package com.example.whereproject;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.whereproject.databinding.ActivityAppBinding;
+
+public class App extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityAppBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+}
--- app/src/main/java/com/example/whereproject/MainActivity.java
+++ app/src/main/java/com/example/whereproject/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.whereproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference mDatabase;
+    private FusedLocationProviderClient mFusedLocationClient;
     Button btnlogin;
     Spinner spinner;
 
@@ -28,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
+        setTheme(R.style.SplashTheme);
 
         ArrayList<CodigoInten> codigo = new ArrayList<CodigoInten>();
         codigo.add(new CodigoInten("+"));
@@ -35,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         codigo.add(new CodigoInten("+562"));
 
 
-        ArrayAdapter<CodigoInten> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,codigo);
+        ArrayAdapter<CodigoInten> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, codigo);
 
         spinner.setAdapter(adapter);
 
@@ -59,6 +78,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    Log.e("Latitud: ", +location.getLatitude()+"Longitud: "+location.getLongitude());
+                    Map<String,Object> latlang = new HashMap<>();
+                    latlang.put("latitud",location.getLatitude());
+                    latlang.put("longitud",location.getLongitude());
+
+                    mDatabase.child("usuarios").push().setValue(latlang);
+                }
+            }
+        });
     }
-    
 }
